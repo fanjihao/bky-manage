020a0610d7295f6a6b1fb97e4bc3ab501c76125a
meng <2562245299@qq.com>
2020-09-12 15:30:09 +0800

    11111

--- src/views/home/Home.js
+++ src/views/home/Home.js
@@ -2,7 +2,7 @@ import React, { Component } from "react"
 import '../home/home.css'
 import { Row, Col } from 'antd'
 import { HomeOutlined, SettingFilled, MenuFoldOutlined } from '@ant-design/icons'
-import { Route } from 'react-router-dom'
+import { Route, NavLink, Redirect } from 'react-router-dom'
 import HomeIndex from '../../component/HomeIndex'
 import Goods from '../goods/Goods'
 import Order from '../order/Order'
@@ -13,23 +13,28 @@ const tab = [
     {
         id: 1,
         name: '首页',
-        icon: <HomeOutlined />
+        icon: <HomeOutlined />,
+        path: '/home/homeindex'
     }, {
         id: 2,
         name: '商品管理',
-        icon: <MenuFoldOutlined />
+        icon: <MenuFoldOutlined />,
+        path: '/home/goods'
     }, {
         id: 3,
         name: '订单管理',
-        icon: 'home'
+        icon: 'home',
+        path: '/home/order'
     }, {
         id: 4,
         name: '员工列表',
-        icon: 'home'
+        icon: 'home',
+        path: '/home/employee'
     }, {
         id: 5,
         name: '设置',
-        icon: <SettingFilled />
+        icon: <SettingFilled />,
+        path: '/home/setting'
     },
 ]
 
@@ -37,7 +42,6 @@ class Home extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            checkCode: 0
         }
     }
 
@@ -49,9 +53,6 @@ class Home extends Component {
     }
 
     render() {
-        const { checkCode } = this.state
-        
-        console.log(checkCode)
         return (
             <div className="home">
                 <Row>
@@ -62,10 +63,10 @@ class Home extends Component {
                         </div>
 
                         {
-                            tab.map((item,index) => {
+                            tab.map((item, index) => {
                                 return (
                                     <div key={item.id} className="tab">
-                                        <div className="tab-items" id = {checkCode === index ? 'check-tab-items' : ''} onClick={() => this.checkTab(index)}>{item.name}</div>
+                                        <NavLink to={item.path} className="tab-items">{item.name}</NavLink>
                                     </div>
                                 )
                             })
@@ -73,11 +74,12 @@ class Home extends Component {
 
                     </Col>
                     <Col span={20} className="home-content">
-                        {checkCode === 0 ? <Route path="/home" component={HomeIndex} /> : ''}
-                        {checkCode === 1 ? <Route path="/home/goods" component={Goods} /> : ''}
-                        {checkCode === 2 ? <Route path="/home/order" component={Order} /> : ''}
-                        {checkCode === 3 ? <Route path="/home/employee" component={Employee} /> : ''}
-                        {checkCode === 4 ? <Route path="/home/setting" component={Setting} /> : ''}
+                        <Redirect from="/home" to="/home/homeindex" />
+                        <Route path="/home/homeindex" component={HomeIndex} />
+                        <Route path="/home/goods" component={Goods} />
+                        <Route path="/home/order" component={Order} />
+                        <Route path="/home/employee" component={Employee} />
+                        <Route path="/home/setting" component={Setting} />
                     </Col>
                 </Row>
             </div>
